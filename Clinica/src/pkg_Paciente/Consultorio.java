e91d788922db7ad28b29fe83d4a68d12f3bf4351
beruon <47423301+beruon@users.noreply.github.com>
2019-03-06 10:46:52 -0500

    Update Consultorio.java

--- Clinica/src/pkg_Paciente/Consultorio.java
+++ Clinica/src/pkg_Paciente/Consultorio.java
@@ -2,9 +2,9 @@ package pkg_Paciente;
 
 public class Consultorio extends Sala{
 
-	public Consultorio(String piso, String numero, String edificio, String tipo, Cita citaAsignada, Paciente pacientes,
+	public Consultorio(String piso, String numero, String edificio, Cita citaAsignada, Paciente pacientes,
 			Doctor doctores) {
-		super(piso, numero, edificio, tipo, citaAsignada, pacientes, doctores);
+		super(piso, numero, edificio, "Consultorio", citaAsignada, pacientes, doctores);
 		// TODO Auto-generated constructor stub
 	}
 
